a41a42cdb9504ff3319d5011bb6f3e5184ef1f17
vuong2 <vuong2@pdx.edu>
2022-07-06 19:56:16 -0700

    finish first part

--- FooBarQix/src/main/java/edu/pdx/cs410J/TeamLucky13/Kata.java
+++ FooBarQix/src/main/java/edu/pdx/cs410J/TeamLucky13/Kata.java
@@ -11,4 +11,31 @@ public class Kata {
   public static void main(String[] args) {
     System.err.println("Missing command line arguments");
   }
+
+  static String compute(String number) {
+    int num = Integer.parseInt(number);
+    StringBuilder result = new StringBuilder();
+
+    if (num % 3 == 0)
+      result.append("Foo");
+    if (num % 5 == 0)
+      result.append("Bar");
+    if (num % 7 == 0)
+      result.append("Qix");
+
+    digitMatch(number, result);
+
+    return result.toString();
+  }
+
+  private static void digitMatch(String number, StringBuilder result) {
+    for (char c : number.toCharArray()) {
+      if (c == '3')
+        result.append("Foo");
+      else if (c == '5')
+        result.append("Bar");
+      else if (c == '7')
+        result.append("Qix");
+    }
+  }
 }
--- FooBarQix/src/test/java/edu/pdx/cs410J/TeamLucky13/KataTest.java
+++ FooBarQix/src/test/java/edu/pdx/cs410J/TeamLucky13/KataTest.java
@@ -1,6 +1,9 @@
 package edu.pdx.cs410J.TeamLucky13;
 
 import org.junit.jupiter.api.Test;
+import static org.hamcrest.CoreMatchers.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class KataTest
 {
@@ -10,4 +13,34 @@ public class KataTest
     new Kata();
   }
 
+  @Test
+  void canPrintOutNumber() {
+    String result = Kata.compute("1");
+    assertThat(result, equalTo("1"));
+  }
+
+  @Test
+  void canPrintOutFoo() {
+    assertThat(Kata.compute("3"), equalTo("Foo"));
+  }
+
+  @Test
+  void canPrintOutBar() {
+    assertThat(Kata.compute("5"), equalTo("Bar"));
+  }
+
+  @Test
+  void canPrintOutQix() {
+    assertThat(Kata.compute("7"), equalTo("Qix"));
+  }
+
+  @Test
+  void canPrintForNumberWithTwoDigits() {
+    assertThat(Kata.compute("21"), equalTo("FooQix"));
+  }
+
+  @Test
+  void divisibleByTwoNumbersAndContainsOne() {
+    assertThat(Kata.compute("15"), equalTo("FooBarBar"));
+  }
 }
